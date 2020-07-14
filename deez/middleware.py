c51e53bf5fd31e7dfe4f4dd12515926cc2575a20
Lemuel Boyce <lemuelboyce@gmail.com>
2020-07-14 18:49:21 -0400

    require middleware methods to return response and request objects
    - remove WildcardDomainMiddleware

--- deez/middleware.py
+++ deez/middleware.py
@@ -3,16 +3,7 @@ class Middleware:
         self.resource = resource
 
     def before_request(self, request):
-        pass
+        return request
 
     def before_response(self, response):
-        pass
-
-
-class WildCardDomainResolverMiddleware(Middleware):
-    def before_request(self, request):
-        domain_name = request.lambda_context.get('domainName')
-        domain_prefix = request.lambda_context.get('domainPrefix')
-        request.request_domain_name = domain_name
-        request.request_domain_prefix = domain_prefix
-        return request
+        return response
