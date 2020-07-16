5e459e320c344be6e23cd6cff61b70e169c525f8
Lemuel Boyce <lemuelboyce@gmail.com>
2020-07-15 20:43:46 -0400

    Require Response type in router - better type hints

--- deez/response.py
+++ deez/response.py
@@ -1,4 +1,6 @@
 import json
+from abc import abstractmethod
+from typing import Optional
 
 
 class Response:
@@ -9,15 +11,16 @@ class Response:
         self.status_code = status_code
         self.content_type = content_type
 
-    def render(self, *args, **kwargs):
-        return self.data
+    @abstractmethod
+    def render(self, *args, **kwargs) -> Optional[str]:
+        pass
 
 
 class NoContentResponse(Response):
     def __init__(self, headers=None):
         super().__init__(headers=headers, status_code=204)
 
-    def render(self, *args, **kwargs):
+    def render(self, *args, **kwargs) -> Optional[str]:
         return None
 
 
@@ -25,5 +28,5 @@ class JsonResponse(Response):
     def __init__(self, data=None, status_code=200, headers=None):
         super().__init__(data=data, status_code=status_code, headers=headers, content_type='application/json')
 
-    def render(self, *args, **kwargs):
+    def render(self, *args, **kwargs) -> Optional[str]:
         return json.dumps(self.data)
