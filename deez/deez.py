237449ac28501f2daa95cd2d869e0a70e97c9acc
Lemuel Boyce <lemuelboyce@gmail.com>
2020-10-08 21:06:47 -0400

    enforce resource types

--- deez/deez.py
+++ deez/deez.py
@@ -1,8 +1,5 @@
-from typing import Union
-
 from deez.conf import Setting
 from deez.router import Router
-from deez.urls import Path
 from deez.utils import resolve_middleware_classes
 
 
@@ -24,9 +21,8 @@ class Deez:
             self.middleware = resolve_middleware_classes(self.settings.MIDDLEWARE)
             self.middleware_reversed = reversed(self.middleware)
 
-    def register_route(self, path: Union[str, Path], resource_class=None) -> None:
+    def register_route(self, path, resource_class=None) -> None:
         self.router.register(path=path, resource=resource_class)
 
     def process_request(self, event, context):
-        assert self.router is not None
         return self.router.route(event, context)
--- deez/urls/__init__.py
+++ deez/urls/__init__.py
@@ -1,4 +1,3 @@
-from deez.logger import get_logger
 from deez.urls.parsers import pattern_replacer
 
 
@@ -8,19 +7,9 @@ class Path:
         self.resource = resource
 
 
-def path(url: str, resource) -> Path:
+def path(url, resource) -> Path:
     regex = pattern_replacer(url)
     return Path(regex, resource)
 
 
-def path_resolver(url: str, resource) -> Path:
-    """
-    Will be deprecated in favor of `path` in
-    future versions.
-    """
-    logger = get_logger()
-    logger.warning("use path instead of path_resolver: %s", url)
-    return path(url, resource)
-
-
-__all__ = [Path, path, path_resolver]
+__all__ = [Path, path]
