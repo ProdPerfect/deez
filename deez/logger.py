b782393b3b2e9438769d1a66b0073c3589366aca
Lemuel Boyce <lemuelboyce@gmail.com>
2020-11-02 23:15:22 -0500

    improvements and configurability

--- deez/logger.py
+++ deez/logger.py
@@ -5,8 +5,7 @@ from functools import lru_cache
 @lru_cache()
 def get_logger():
     from deez.conf import settings
-    fmt = '%(asctime)s %(levelname)s %(message)s'
-    logging.basicConfig(format=fmt)
+    logging.basicConfig(format=settings.LOGGER_MESSAGE_FORMAT)
     logger = logging.getLogger('deez')
     if settings.DEBUG:
         logger.setLevel(logging.DEBUG)
