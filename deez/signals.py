9dfd2883bd1d83515df321d823b9cba72df17e7e
Lemuel Boyce <lemuelboyce@gmail.com>
2021-05-17 17:37:20 -0400

    ignore mypy linting errors for blinker as it does not provide stubs
    Signed-off-by: Lemuel Boyce <lemuelboyce@gmail.com>

--- deez/signals.py
+++ deez/signals.py
@@ -1,4 +1,4 @@
-from blinker import signal
+from blinker import signal  # type: ignore
 
 application_setup_started = signal('application-setup-started')
 application_setup_finished = signal('application-setup-finished')
